27d21edcf14ea4790166eea36deae05861e8655e
acmpo6ou <acmpo6ou@gmail.com>
2022-01-05 09:43:56 +0200

    🔌 Write stub for Wire class

--- circuits.py
+++ circuits.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
+
+
+@dataclass
+class Wire:
+    repr: str
+    color: str
+
+    grid: list = None
+    powered = False
+
+    def is_connected(self, neighbor: "Wire", side):
+        ...
+
+    @property
+    def neighbors(self) -> list["Wire"]:
+        ...
+
+    def evaluate_power(self):
+        ...
 
 
 def main():
