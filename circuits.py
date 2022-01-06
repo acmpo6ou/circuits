43e2a23c42f9b7d51a86c3ec71cb4c9df5fc015b
acmpo6ou <acmpo6ou@gmail.com>
2022-01-06 10:12:37 +0200

    🪛 Write grid for testing

--- circuits.py
+++ circuits.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+from wire import Wire
+
+# fmt: off
+grid = [
+    [Wire("┼", "red"), Wire("┼", "blue"), Wire("─", "blue"), Wire("─", "blue"), Wire("┤", "blue")],
+    [Wire("├", "red"), Wire("┼", "red"), Wire("┼", "blue"), Wire("┼", "blue"), Wire("┤", "blue")],
+    [Wire("├", "red"), Wire("┼", "red"), Wire("┼", "blue"), Wire("┼", "blue"), Wire("┤", "blue")],
+    [Wire("├", "red"), Wire("┼", "red"), Wire("┼", "red"), Wire("┼", "blue"), Wire("┤", "blue")],
+    [Wire("├", "red"), Wire("─", "red"), Wire("─", "red"), Wire("─", "red"), Wire("┤", "blue")],
+]
+# fmt: on
+
+for row in grid:
+    for wire in row:
+        wire.grid = grid
 
 
 def main():
