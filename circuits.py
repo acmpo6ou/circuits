078ff2a305a2f90c08aea40c01758fa38be230ae
acmpo6ou <acmpo6ou@gmail.com>
2022-01-06 10:36:44 +0200

    ⚙️  Write draft implementation of algorithm

--- circuits.py
+++ circuits.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from copy import deepcopy
+
 from wire import Wire
 
 # fmt: off
@@ -15,9 +17,30 @@ for row in grid:
     for wire in row:
         wire.grid = grid
 
+grid[0][0].powered = True
+grid[0][1].powered = True
+
+old_state = None
+
 
 def main():
-    ...
+    global old_state
+
+    while True:
+        old_state = deepcopy(grid)
+        for row in grid:
+            for wire in row:
+                if wire.position() in ((0, 0), (0, 1)):
+                    continue
+                wire.evaluate_power()
+
+        if old_state == grid:
+            break
+
+    for row in grid:
+        for wire in row:
+            wire.print()
+        print()
 
 
 if __name__ == "__main__":
