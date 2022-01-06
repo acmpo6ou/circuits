03de8b6aefbb0717ee864004bf5928c1ab367ae7
acmpo6ou <acmpo6ou@gmail.com>
2022-01-06 14:05:14 +0200

    💡 Implement Lamp

---
+++ lamp.py
@@ -0,0 +1,29 @@
+from wire import Wire, Side, sides
+
+
+class Lamp(Wire):
+    sides = sides["┼"]
+
+    def __init__(self):
+        super().__init__("💡", "")
+
+    @property
+    def neighbors(self) -> list["Wire"]:
+        x, y = self.position()
+        neighbors = []
+
+        for side in Side:
+            dx, dy = side.value
+            try:
+                neighbor = self.grid[y + dy][x + dx]
+                if not self.is_connected(neighbor, side):
+                    continue
+            except IndexError:
+                continue
+            neighbors.append(neighbor)
+        return neighbors
+
+    def evaluate_power(self):
+        red = any(neighbor.powered for neighbor in self.neighbors if neighbor.color == "red")
+        blue = any(neighbor.powered for neighbor in self.neighbors if neighbor.color == "blue")
+        self.powered = red and blue
