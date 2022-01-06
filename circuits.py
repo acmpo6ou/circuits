bd2aa6a4eedba866db51abd15819d75937156322
acmpo6ou <acmpo6ou@gmail.com>
2022-01-06 14:32:17 +0200

    ⚙️  Implement gengrid

--- circuits.py
+++ circuits.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
+import random
 from copy import deepcopy
 
 from core.lamp import Lamp
 from core.plus_minus import PlusMinus
-from core.wire import Wire
-
-# fmt: off
-grid = [
-    [PlusMinus("+"), PlusMinus("-"), Wire("─", "blue"), Wire("─", "blue"), Wire("┤", "blue")],
-    [Wire("├", "red"), Wire("┼", "red"), Wire("┼", "blue"), Wire("┼", "blue"), Wire("┤", "blue")],
-    [Wire("├", "red"), Wire("┼", "red"), Wire("┼", "blue"), Wire("┼", "blue"), Wire("┤", "blue")],
-    [Wire("├", "red"), Wire("┼", "red"), Wire("┼", "red"), Wire("┼", "blue"), Wire("┤", "blue")],
-    [Wire("├", "red"), Wire("─", "red"), Wire("─", "red"), Wire("─", "red"), Lamp()],
-]
-# fmt: on
-
-for row in grid:
-    for wire in row:
-        wire.grid = grid
+from core.wire import Wire, sides
 
 old_state = None
+grid = []
+
+
+def gengrid():
+    for _ in range(5):
+        row = []
+        for _ in range(5):
+            color = random.choice(("red", "blue"))
+            wire = random.choice(list(sides.keys()))
+            row.append(Wire(wire, color))
+        grid.append(row)
+
+    px, py, mx, my, lx, ly = random.choices(range(5), k=6)
+    grid[px][py] = PlusMinus("+")
+    grid[mx][my] = PlusMinus("-")
+    grid[lx][ly] = Lamp()
+
+    for row in grid:
+        for wire in row:
+            wire.grid = grid
 
 
 def main():
+    gengrid()
     global old_state
 
     while True:
